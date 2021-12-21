cfe9646a18618867add5a2087905ac55cf03fac2
developerabs <60730088+developerabs@users.noreply.github.com>
2021-12-21 18:06:48 +0600

    installation

---
+++ vendor/mehedi-iitdu/core-component-repository/src/CoreComponentRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MehediIitdu\CoreComponentRepository;
+use App\Models\Addon;
+use Cache;
+
+class CoreComponentRepository
+{
+    public static function instantiateShopRepository() {
+        return;
+    }
+
+    protected static function serializeObjectResponse($zn) {
+        return;
+    }
+
+    protected static function finalizeRepository($rn) {
+        return;
+    }
+
+    public static function initializeCache() {
+        return;
+    }
+
+    public static function finalizeCache($addon){
+        return;
+    } 
+}
